9e3ca11553576fc435fa57731e4fa7a98212e56a
maha <12mahich99@mail.ru>
2023-03-29 17:19:16 +0300

    footer

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,13 @@
+import "./Footer.css"
+
+function Footer(){
+    return(
+        <div className="footer">
+            <a href="#">Privacy</a>
+            <a href="#">Moscow</a>
+            <a href="#">Instagram</a>
+        </div>
+    );
+}
+
+export default Footer;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import Footer from '../components/footer/Footer'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>Home
+        <Footer></Footer>
+
+    </div>
   )
 }
 
